30e3a496b9dcd19d05567283f0e4cbfd3dad5e7b
naman-ng <naman21171@iiitd.ac.in>
2022-12-27 15:35:18 +0530

    Add: Connect Wallet

--- hardhat/scripts/run.js
+++ hardhat/scripts/run.js
@@ -3,6 +3,7 @@ const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory(
         'Domains'
     );
+
     const domainContract = await domainContractFactory.deploy('mind');
     await domainContract.deployed();
     console.log('Contract deployed to: ', domainContract.address);
